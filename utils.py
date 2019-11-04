bf21d5f18a8de16e08da1eb7b871d56eca6aa562
nox1202 <43510708+nox1202@users.noreply.github.com>
2019-11-04 17:17:28 +1100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 """
 Some codes from https://github.com/Newmu/dcgan_code
 """
-from __future__ import division
+
 import math
 import json
 import random
@@ -177,8 +177,8 @@ def visualize(sess, dcgan, config, option):
     save_images(samples, [image_frame_dim, image_frame_dim], './samples/test_%s.png' % strftime("%Y%m%d%H%M%S", gmtime()))
   elif option == 1:
     values = np.arange(0, 1, 1./config.batch_size)
-    for idx in xrange(100):
-      print(" [*] %d" % idx)
+    for idx in range(100):
+      print((" [*] %d" % idx))
       z_sample = np.zeros([config.batch_size, dcgan.z_dim])
       for kdx, z in enumerate(z_sample):
         z[idx] = values[kdx]
@@ -195,8 +195,8 @@ def visualize(sess, dcgan, config, option):
       save_images(samples, [image_frame_dim, image_frame_dim], './samples/test_arange_%s.png' % (idx))
   elif option == 2:
     values = np.arange(0, 1, 1./config.batch_size)
-    for idx in [random.randint(0, 99) for _ in xrange(100)]:
-      print(" [*] %d" % idx)
+    for idx in [random.randint(0, 99) for _ in range(100)]:
+      print((" [*] %d" % idx))
       z = np.random.uniform(-0.2, 0.2, size=(dcgan.z_dim))
       z_sample = np.tile(z, (config.batch_size, 1))
       #z_sample = np.zeros([config.batch_size, dcgan.z_dim])
@@ -218,8 +218,8 @@ def visualize(sess, dcgan, config, option):
         save_images(samples, [image_frame_dim, image_frame_dim], './samples/test_%s.png' % strftime("%Y%m%d%H%M%S", gmtime()))
   elif option == 3:
     values = np.arange(0, 1, 1./config.batch_size)
-    for idx in xrange(100):
-      print(" [*] %d" % idx)
+    for idx in range(100):
+      print((" [*] %d" % idx))
       z_sample = np.zeros([config.batch_size, dcgan.z_dim])
       for kdx, z in enumerate(z_sample):
         z[idx] = values[kdx]
@@ -230,8 +230,8 @@ def visualize(sess, dcgan, config, option):
     image_set = []
     values = np.arange(0, 1, 1./config.batch_size)
 
-    for idx in xrange(100):
-      print(" [*] %d" % idx)
+    for idx in range(100):
+      print((" [*] %d" % idx))
       z_sample = np.zeros([config.batch_size, dcgan.z_dim])
       for kdx, z in enumerate(z_sample): z[idx] = values[kdx]
 
@@ -239,5 +239,5 @@ def visualize(sess, dcgan, config, option):
       make_gif(image_set[-1], './samples/test_gif_%s.gif' % (idx))
 
     new_image_set = [merge(np.array([images[idx] for images in image_set]), [10, 10]) \
-        for idx in range(64) + range(63, -1, -1)]
+        for idx in list(range(64)) + list(range(63, -1, -1))]
     make_gif(new_image_set, './samples/test_gif_merged.gif', duration=8)
